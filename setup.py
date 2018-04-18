00451d8baf4235aada3488a63bdace558cd161de
SimonGreenhill <simon@simon.net.nz>
2018-04-18 09:12:08 +0200

    replacing attr with attrs

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ setup(
     python_requires='>=2.7,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*',
     install_requires=[
         'six',
-        'attr',
+        'attrs',
         'pycldf',
         'clldutils',
         'pyglottolog',
