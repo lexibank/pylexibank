4ce21502f433032240c282adccb965d26675c5ae
xrotwang <xrotwang@googlemail.com>
2018-05-07 20:56:52 +0200

    release 0.7.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pylexibank',
-    version='0.6.1.dev0',
+    version='0.7.0',
     author='Robert Forkel',
     author_email='forkel@shh.mpg.de',
     description='Python library implementing the lexibank workbench',
