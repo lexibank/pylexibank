b0f8ff5b0e3c898d8dd6c07d6b0361cc370faa2a
SimonGreenhill <simon@simon.net.nz>
2020-01-28 11:31:58 +0100

    fix logging error

--- src/pylexibank/commands/check_languages.py
+++ src/pylexibank/commands/check_languages.py
@@ -29,7 +29,7 @@ def check(ds, args):
         l.id for l in args.glottolog.api.languoids() if l.lineage and l.lineage[0][1] == 'book1242')
     for l in cldf['LanguageTable']:
         if l['Glottocode'] and l['Glottocode'] in bookkeeping:
-            args.log(
+            args.log.warn(
                 'Language {0} mapped to Bookkeeping languoid {1}'.format(l['ID'], l['Glottocode']))
 
 
