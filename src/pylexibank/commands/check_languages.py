526e6368eacf2d9b11ada23fbb8bb8cfc6e581dd
xrotwang <xrotwang@googlemail.com>
2020-01-28 08:19:28 +0100

    Skeleton for language checks
    To run on all installed lexibank datasets:
    ```
    $ cldfbench lexibank.check_languages _ --glottolog-version v4.1
    WARNING Error importing naganorgyalrongic: cannot import name 'NonSplittingDataset'
    INFO    running check on normansinitic ...
    INFO    checking Dataset "normansinitic" at /home/forkel/venvs/cldf/normansinitic
    WARNING Dataset compiled against Glottolog v4.0
    INFO    ... done normansinitic [12.4 secs]
    ```

---
+++ src/pylexibank/commands/check_languages.py
@@ -0,0 +1,37 @@
+"""
+Check language specifications of lexibank datasets
+"""
+
+from cldfbench.cli_util import with_datasets, add_catalog_spec
+from pylexibank.cli_util import add_dataset_spec
+
+
+def register(parser):
+    add_dataset_spec(parser, multiple=True)
+    add_catalog_spec(parser, 'glottolog')
+
+
+def get_glottolog_version(cldf):
+    for repo in cldf.properties.get("prov:wasDerivedFrom", []):
+        if repo.get('dc:title') == 'Glottolog':
+            return repo.get('dc:created')
+
+
+def check(ds, args):
+    args.log.info('checking {0}'.format(ds))
+    cldf = ds.cldf_reader()
+    if args.glottolog_version:
+        gv = get_glottolog_version(cldf)
+        if gv != args.glottolog_version:
+            args.log.warn('Dataset compiled against Glottolog {0}'.format(gv))
+
+    bookkeeping = set(
+        l.id for l in args.glottolog.api.languoids() if l.lineage and l.lineage[0][1] == 'book1242')
+    for l in cldf['LanguageTable']:
+        if l['Glottocode'] and l['Glottocode'] in bookkeeping:
+            args.log(
+                'Language {0} mapped to Bookkeeping languoid {1}'.format(l['ID'], l['Glottocode']))
+
+
+def run(args):
+    with_datasets(args, check)
