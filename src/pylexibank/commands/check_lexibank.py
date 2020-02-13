2108815ab0bc814f6aac0fb4581b0096152f6f73
SimonGreenhill <simon@simon.net.nz>
2020-02-13 21:35:49 +0100

    adding check for lexeme

--- src/pylexibank/commands/check_lexibank.py
+++ src/pylexibank/commands/check_lexibank.py
@@ -2,7 +2,6 @@
 Check lexibank plumbing for lexibank datasets
 """
 import functools
-
 from cldfbench.cli_util import with_datasets, add_catalog_spec
 from pylexibank.cli_util import add_dataset_spec, warning
 
@@ -24,6 +23,14 @@ def check(ds, args):
     for field in ['citation', 'title', 'license']:
         if getattr(ds.metadata, field, None) in (None, ""):
             warn("Dataset has an empty '{0}' in metadata file".format(field))
+
+    # check lexemes.csv
+    if (ds.dir / 'etc' / 'lexemes.csv').exists():
+        cldf = ds.cldf_reader()
+        values = set(f['Value'] for f in cldf['FormTable'])
+        for r in ds.lexemes:
+            if r not in values:
+                warn("lexemes.csv contains un-needed conversion '{0}'".format(r))
 
 
 def run(args):
