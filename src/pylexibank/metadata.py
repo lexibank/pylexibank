f38ec974f97e8a2ddd547267b56da63a86b6c263
xrotwang <xrotwang@googlemail.com>
2020-04-15 15:40:27 +0200

    closes #213

--- src/pylexibank/metadata.py
+++ src/pylexibank/metadata.py
@@ -1,11 +1,39 @@
+import re
 import pkg_resources
 
 import attr
 from cldfbench.metadata import Metadata
 
-__all__ = ['LexibankMetadata']
+__all__ = ['LexibankMetadata', 'check_standard_title']
 
 version = pkg_resources.get_distribution('pylexibank').version
+
+STANDARD_TITLE_PATTERN = re.compile(
+    r'CLDF dataset derived from\s+'
+    r'(?P<authors>[^"]+)'
+    r'"(?P<title>[^"]+)"\s+from\s+'
+    r'(?P<year>[0-9]{4})'
+)
+
+
+def check_standard_title(title):
+    """
+    Assert a title conforms to the standard format.
+
+    > CLDF dataset derived from AUTHOR's "TITLE" from YEAR
+
+    Usage: In a dataset's `test.py`:
+    ```python
+    from pylexibank import check_standard_title
+
+    def test_valid_title(cldf_dataset, cldf_logger):
+        check_standard_title(cldf_dataset.metadata_dict['dc:title'])
+    ```
+
+    Note that this requires installing `pylexibank` for tests, also in .travis.yml.
+    """
+    match = STANDARD_TITLE_PATTERN.fullmatch(title)
+    assert match and match.group('authors').strip().endswith("'s")
 
 
 @attr.s
--- tests/test_metadata.py
+++ tests/test_metadata.py
@@ -1,3 +1,5 @@
+import pytest
+
 from pylexibank.metadata import *
 
 
@@ -5,3 +7,23 @@ def test_conceptlist():
     md = LexibankMetadata(conceptlist='Swadesh-1964-100', related='abc')
     assert isinstance(md.conceptlist, list)
     assert 'concepticon.clld.org' in md.markdown()
+
+
+STANDARD_TITLE = """CLDF dataset derived from A et al.'s "Paper" from 1934"""
+
+
+@pytest.mark.parametrize('title', [
+    "",
+    STANDARD_TITLE.replace('CLDF', 'cldf'),
+    STANDARD_TITLE.replace("et al.'s", 'et al.’s'),
+    STANDARD_TITLE.replace('"', "'"),
+    STANDARD_TITLE.replace('1934', '34'),
+])
+def test_invalid_standard_title(title):
+    with pytest.raises(AssertionError):
+        check_standard_title(title)
+
+
+def test_valid_standard_title():
+    assert check_standard_title(STANDARD_TITLE) is None
+
