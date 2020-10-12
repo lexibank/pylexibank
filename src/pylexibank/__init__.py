dd81f7d9a9d18b8e4f77bffc169d6f6206ee104b
xrotwang <xrotwang@googlemail.com>
2020-10-12 21:09:22 +0200

    bump version for development

--- src/pylexibank/__init__.py
+++ src/pylexibank/__init__.py
@@ -6,4 +6,4 @@ from pylexibank.metadata import *  # noqa: F401, F403
 from pylexibank.util import *  # noqa: F401, F403
 from pylexibank.cldf import *  # noqa: F401, F403
 
-__version__ = '2.7.2'
+__version__ = '2.7.3.dev0'
