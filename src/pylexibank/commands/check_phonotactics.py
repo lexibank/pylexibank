184bc2a7b0d1e92684d94b4fb3220dcfb779dd6b
lingulist <mattis.list@lingpy.org>
2020-11-25 15:46:27 +0100

    add prrosody check

--- src/pylexibank/commands/check_phonotactics.py
+++ src/pylexibank/commands/check_phonotactics.py
@@ -1,49 +1,45 @@
 """
-Check phonotactics of segmented data.
+Check forms against a dataset's orthography profile.
 """
-import collections
-
-from cldfbench.cli_util import with_dataset
-from clldutils import markup
-
+from cldfbench.cli_util import with_dataset, add_catalog_spec
 from pylexibank.cli_util import add_dataset_spec
-
+from tabulate import tabulate
 
 def register(parser):
     add_dataset_spec(parser)
 
 
 def run(args):
-    with_dataset(args, check_phonotactics)
+    with_dataset(args, check_segments)
 
 
-def table(problems):
-    table_ = markup.Table('No', 'ID', 'Form', 'Segments')
-    for i, (a, b, c) in enumerate(problems, start=1):
-        table_.append((i, a, b, c))
-    return table_.render(tablefmt='pipe')
-
-
-def check_phonotactics(dataset, args):
-    problems = collections.defaultdict(list)
+def check_segments(dataset, args):
+    problems, visited = set(), set()
+    table = []
     for row in dataset.cldf_dir.read_csv('forms.csv', dicts=True):
-        tokens = row['Segments'].split()
-        # trailing plusses
-        if tokens[-1] == '+':
-            problems['+$'] += [(row['ID'], row['Form'], row['Segments'])]
-        if tokens[0] == '+':
-            problems['^+'] += [(row['ID'], row['Form'], row['Segments'])]
-        if '+ +' in row['Segments']:
-            problems['++'] += [(row['ID'], row['Form'], row['Segments'])]
-    if not problems:  # pragma: no cover
-        print('Found no errors.')
-        return
-    if '+$' in problems:
-        print('# Segments end in +:')
-        print(table(problems['+$']))
-    if '^+' in problems:
-        print('# Segments start with +:')
-        print(table(problems['^+']))
-    if '++' in problems:
-        print('# Segments have consecutive +:')
-        print(table(problems['++']))
+        tokens = row['Segments']
+        tkl = row['Segments'].split()
+        if tkl:
+            if (
+                    tkl[0] in ['+', '_', '#'] or 
+                    tkl[-1] in ['+', '_', '#'] or 
+                    '+ +' in tokens or 
+                    '_ _' in tokens or 
+                    '#' in tokens or
+                    '_' in tokens
+                    ):
+                table += [[
+                    1,
+                    row['ID'],
+                    row['Value'],
+                    row['Form'],
+                    row['Graphemes'],
+                    row['Segments']]]
+        else:
+            table += [[
+                2, row['ID'], row['Value'], row['Form'], row['Graphemes'], 
+                row['Segments']]]
+    print(tabulate(
+        sorted(table, key=lambda x: (x[1], x[0])),
+        headers=['Type', 'ID', 'Value', 'Form', 'Graphemes', 'Segments'],
+        tablefmt='pipe'))
