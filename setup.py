26cf39c285df79f23c8cba0af000c748c8836810
Robert Forkel <xrotwang@googlemail.com>
2021-04-21 19:01:27 +0200

    bump version for development

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pylexibank',
-    version='3.1.0',
+    version='3.1.1.dev0',
     author='Robert Forkel',
     author_email='dlce.rdm@eva.mpg.de',
     description='Python library implementing the lexibank workbench',
