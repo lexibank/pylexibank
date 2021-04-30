768939f7f2ad161957fbba3aa4070609e1835a8d
lingulist <mattis.list@lingpy.org>
2021-04-30 20:46:09 +0200

    refine style

--- src/pylexibank/commands/check_profile.py
+++ src/pylexibank/commands/check_profile.py
@@ -7,9 +7,9 @@ from pylexibank.cli_util import add_dataset_spec
 
 
 def register(parser):
-    add_format(parser, default='pipe')
+    add_format(parser, default="pipe")
     add_dataset_spec(parser)
-    add_catalog_spec(parser, 'clts')
+    add_catalog_spec(parser, "clts")
 
 
 def run(args):
@@ -19,101 +19,103 @@ def run(args):
 def check_profile(dataset, args):
     visited = {}
     missing, unknown, modified, generated = {}, {}, {}, {}
-    for row in dataset.cldf_dir.read_csv('forms.csv', dicts=True):
-        tokens = dataset.tokenizer(row, row['Form'], column='IPA') if \
-            dataset.tokenizer else row['Segments'].split()
-        for tk in set(tokens): 
+    for row in dataset.cldf_dir.read_csv("forms.csv", dicts=True):
+        tokens = (
+            dataset.tokenizer(row, row["Form"], column="IPA")
+            if dataset.tokenizer
+            else row["Segments"].split()
+        )
+        for tk in set(tokens):
             if tk not in visited:
                 sound = args.clts.api.bipa[tk]
-                if tk.startswith('<<') and tk.endswith('>>'):
+                if tk.startswith("<<") and tk.endswith(">>"):
                     visited[tk] = "missing"
                     missing[tk[2:-2]] = [
-                            (" ".join(tokens), row["Form"], row["Graphemes"])]
-                elif sound.type == 'unknownsound':
+                        (" ".join(tokens), row["Form"], row["Graphemes"])
+                    ]
+                elif sound.type == "unknownsound":
                     visited[tk] = "unknown"
-                    unknown[tk] = [
-                            (" ".join(tokens), row["Form"], row["Graphemes"])]
+                    unknown[tk] = [(" ".join(tokens), row["Form"], row["Graphemes"])]
                 elif sound.generated:
                     visited[tk] = "generated"
-                    generated[tk] = [
-                            (" ".join(tokens), row["Form"], row["Graphemes"])]
+                    generated[tk] = [(" ".join(tokens), row["Form"], row["Graphemes"])]
                 elif str(sound) != tk:
                     visited[tk] = "modified"
-                    modified[tk] = [
-                            (" ".join(tokens), row["Form"],
-                                row["Graphemes"])]
+                    modified[tk] = [(" ".join(tokens), row["Form"], row["Graphemes"])]
             else:
                 if visited[tk] == "missing":
-                    missing[tk[2:-2]] += [(
-                        " ".join(tokens), row["Form"], row["Graphemes"])]
+                    missing[tk[2:-2]] += [
+                        (" ".join(tokens), row["Form"], row["Graphemes"])
+                    ]
                 elif visited[tk] == "unknown":
-                    unknown[tk] += [(
-                        " ".join(tokens), row["Form"], row["Graphemes"])]
+                    unknown[tk] += [(" ".join(tokens), row["Form"], row["Graphemes"])]
                 elif visited[tk] == "modified":
-                    modified[tk] += [(
-                        " ".join(tokens), row["Form"], row["Graphemes"])]
+                    modified[tk] += [(" ".join(tokens), row["Form"], row["Graphemes"])]
                 elif visited[tk] == "generated":
-                    generated[tk] += [(
-                        " ".join(tokens), row["Form"], row["Graphemes"])]
-    
+                    generated[tk] += [(" ".join(tokens), row["Form"], row["Graphemes"])]
+
     if generated:
         print("# Found {0} generated graphemes".format(len(generated)))
         with Table(
-                args, 
-                *["Grapheme", "BIPA", "Modified", "Segments", "Graphemes", "Count"]
-                ) as table:
-            for tk, values in sorted(
-                    generated.items(), key=lambda x: len(x[1])):
-                table.append([
-                    tk,
-                    str(args.clts.api.bipa[tk]),
-                    "*" if tk != str(args.clts.api.bipa[tk]) else "",
-                    values[0][0],
-                    values[0][1],
-                    len(values)])
+            args, *["Grapheme", "BIPA", "Modified", "Segments", "Graphemes", "Count"]
+        ) as table:
+            for tk, values in sorted(generated.items(), key=lambda x: len(x[1])):
+                table.append(
+                    [
+                        tk,
+                        str(args.clts.api.bipa[tk]),
+                        "*" if tk != str(args.clts.api.bipa[tk]) else "",
+                        values[0][0],
+                        values[0][1],
+                        len(values),
+                    ]
+                )
     if modified:
         print("# Found {0} modified graphemes".format(len(modified)))
         with Table(
-                args, 
-                *["Grapheme", "BIPA", "Segments", "Graphemes", "Count"]
-                ) as table:
-            for tk, values in sorted(
-                    modified.items(), key=lambda x: len(x[1])):
-                table.append([
-                    tk,
-                    str(args.clts.api.bipa[tk]),
-                    values[0][0],
-                    values[0][1],
-                    len(values)])
+            args, *["Grapheme", "BIPA", "Segments", "Graphemes", "Count"]
+        ) as table:
+            for tk, values in sorted(modified.items(), key=lambda x: len(x[1])):
+                table.append(
+                    [
+                        tk,
+                        str(args.clts.api.bipa[tk]),
+                        values[0][0],
+                        values[0][1],
+                        len(values),
+                    ]
+                )
     if unknown:
         print("# Found {0} unknown graphemes".format(len(unknown)))
         with Table(
-                args, 
-                *["Grapheme", "Diacritics", "Unicode", "Segments", "Graphemes", "Count"]
-                ) as table:
-            for tk, values in sorted(
-                    unknown.items(), key=lambda x: len(x[1])):
-                table.append([
-                    tk,
-                    "◌"+tk,
-                    args.clts.api.bipa[tk].codepoints,
-                    values[0][0],
-                    values[0][1],
-                    len(values)])
+            args,
+            *["Grapheme", "Diacritics", "Unicode", "Segments", "Graphemes", "Count"]
+        ) as table:
+            for tk, values in sorted(unknown.items(), key=lambda x: len(x[1])):
+                table.append(
+                    [
+                        tk,
+                        "◌" + tk,
+                        args.clts.api.bipa[tk].codepoints,
+                        values[0][0],
+                        values[0][1],
+                        len(values),
+                    ]
+                )
     if missing:
         print("# Found {0} graphemes missing in profile".format(len(unknown)))
         with Table(
-                args, 
-                *["Grapheme", "Diacritics", "Unicode", "Segments", "Graphemes", "Count"]
-                ) as table:
-            for tk, values in sorted(
-                    missing.items(), key=lambda x: len(x[1])):
-                table.append([
-                    tk,
-                    "◌"+tk,
-                    args.clts.api.bipa[tk].codepoints,
-                    values[0][0],
-                    values[0][1],
-                    len(values)])
-
-
+            args,
+            *["Grapheme", "Diacritics", "Unicode", "Segments", "Graphemes", "Count"]
+        ) as table:
+            for tk, values in sorted(missing.items(), key=lambda x: len(x[1])):
+                table.append(
+                    [
+                        tk,
+                        "◌" + tk,
+                        args.clts.api.bipa[tk].codepoints,
+                        values[0][0],
+                        values[0][1],
+                        len(values),
+                    ]
+                )
