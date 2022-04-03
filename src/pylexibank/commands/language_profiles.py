839d510f94d038d7042ddf8d1cd1af44c6bee3f3
lingulist <mattis.list@lingulist.de>
2022-04-03 12:41:22 +0200

    add language-profiles

---
+++ src/pylexibank/commands/language_profiles.py
@@ -0,0 +1,47 @@
+"""
+Create a profile for individual languages from an already prepared general profile.
+"""
+from cldfbench.cli_util import get_dataset
+from clldutils.clilib import ParserError
+from pycldf import Dataset
+
+from pylexibank.cli_util import add_dataset_spec
+from pathlib import Path
+from collections import defaultdict
+from pylexibank import progressbar
+
+
+def register(parser):
+    add_dataset_spec(parser)
+    parser.add_argument(
+        '-f', '--force',
+        action='store_true',
+        help='Overwrite existing profile',
+        default=False)
+
+
+def run(args):
+
+    ds = get_dataset(args)
+    wordlist = Dataset.from_metadata(ds.cldf_dir / "cldf-metadata.json")
+    
+    for language in progressbar(
+            wordlist.objects("LanguageTable"), 
+            desc="creating profiles"):
+        data = defaultdict(int)
+        for form in language.forms:
+            for grapheme, segment in zip(form.data["Graphemes"], form.data["Segments"]):
+                data[(grapheme, segment)] += 1
+        new_path = ds.etc_dir / "orthography" / "{0}.tsv".format(language.id)
+        if new_path.exists() and not args.force:
+            raise ParserError("Orthography profile exists, use --force to override")
+        with open(
+                new_path,
+                "w",
+                encoding="utf-8") as f:
+            f.write("{0}\t{1}\t{2}\n".format("Grapheme", "IPA", "Frequency"))
+            for (g, s), freq in sorted(
+                    data.items(), key=lambda x: x[1],
+                    reverse=True):
+                f.write("{0}\t{1}\t{2}\n".format(g, s, freq))
+
