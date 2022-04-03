576a7173b9b6daec2c92da300441023e26ec7de0
lingulist <mattis.list@lingulist.de>
2022-04-03 12:50:20 +0200

    update

--- src/pylexibank/commands/language_profiles.py
+++ src/pylexibank/commands/language_profiles.py
@@ -9,6 +9,7 @@ from pylexibank.cli_util import add_dataset_spec
 from pathlib import Path
 from collections import defaultdict
 from pylexibank import progressbar
+from csvw.dsv import UnicodeDictReader
 
 
 def register(parser):
@@ -24,14 +25,22 @@ def run(args):
 
     ds = get_dataset(args)
     wordlist = Dataset.from_metadata(ds.cldf_dir / "cldf-metadata.json")
+    p = ds.etc_dir / "orthography.tsv"
+    if not p.exists():
+        raise ParserError("profile does not exist but is needed for creation")
+    with UnicodeDictReader(p, delimiter="\t") as reader:
+        profile = {}
+        for row in reader:
+            profile[row["Grapheme"]] = row["IPA"]
+
     
     for language in progressbar(
             wordlist.objects("LanguageTable"), 
             desc="creating profiles"):
         data = defaultdict(int)
         for form in language.forms:
-            for grapheme, segment in zip(form.data["Graphemes"], form.data["Segments"]):
-                data[(grapheme, segment)] += 1
+            for grapheme in form.data["Graphemes"]:
+                data[grapheme, profile.get(grapheme, "?")] += 1
         new_path = ds.etc_dir / "orthography" / "{0}.tsv".format(language.id)
         if new_path.exists() and not args.force:
             raise ParserError("Orthography profile exists, use --force to override")
