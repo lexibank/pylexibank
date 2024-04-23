b8c203d9843f72bdf7dc22b0ad9ccd1f9950d1d6
chrzyki <christoph@functor-argument.net>
2024-04-23 14:44:17 +0200

    Check for potentially problematic consonant clusters

---
+++ src/pylexibank/commands/consonant_clusters.py
@@ -0,0 +1,50 @@
+"""
+Check for (potentially) problematic consonant clusters > 2.
+"""
+
+from clldutils.clilib import Table, add_format
+from cldfbench.cli_util import with_dataset, add_catalog_spec
+from pylexibank.cli_util import add_dataset_spec
+import collections
+
+
+def register(parser):
+    add_dataset_spec(parser)
+    add_catalog_spec(parser, "clts")
+    add_format(parser, default="pipe")
+
+
+def run(args):
+    with_dataset(args, collect_consonant_clusters)
+
+
+def compute_consonant_cluster(word, sound_names):
+    out = [[]] if sound_names[0].split(" ")[-1] in ["consonant", "cluster"] else []
+    for i, sound in enumerate(sound_names):
+        if sound.split(" ")[-1] in ["diphthong", "vowel", "tone", "�", "marker"]:
+            out += [[]]
+        else:
+            out[-1] += [word[i]]
+    return [chunk for chunk in out if chunk]
+
+
+def collect_consonant_clusters(dataset, args):
+    by_lang = collections.defaultdict(lambda: collections.defaultdict(list))
+
+    with Table(args, "Language_ID", "Length", "Cluster", "Words") as table:
+        for row in sorted(
+            dataset.cldf_dir.read_csv("forms.csv", dicts=True), key=lambda r: r["ID"]
+        ):
+            segments = row["Segments"].split(" + ")
+            for morpheme, sounds in map(
+                lambda x: (x.split(), [s.name for s in args.clts.api.bipa(x.split())]), segments
+            ):
+                clusters = compute_consonant_cluster(morpheme, sounds)
+                for cluster in clusters:
+                    by_lang[tuple(cluster)][row["Language_ID"]] += [row["Form"]]
+
+        for cluster in sorted(by_lang, key=lambda x: len(x)):
+            data = by_lang[cluster]
+            if len(cluster) > 2:
+                for language, words in data.items():
+                    table.append([language, str(len(cluster)), " ".join(cluster), " // ".join(words)])
